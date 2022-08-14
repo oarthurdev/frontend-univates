b4f8f1c0c4c58062e432f934640ea8714d350b2e
Arthur Wagenknecht <arthur.wagenknecht@itmapps.com.br>
2022-08-13 23:54:56 -0300

    Added transition in router view

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -58,9 +58,15 @@
     </q-drawer>
 
     <q-page-container>
-      <transition name="fade-in-right">
-        <router-view />
-      </transition>
+      <router-view v-slot="{ Component }">
+        <transition
+          appear
+          enter-active-class="animated fadeIn"
+          leave-active-class="animated fadeOut"
+        >
+          <component :is="Component" />
+        </transition>
+      </router-view>
     </q-page-container>
   </q-layout>
 </template>
